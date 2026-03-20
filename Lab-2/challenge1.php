055f164a2f45f1a99b4debaab35d2a4428ead0c1
Faye Camille Buri <152823567+fayecamilleburi@users.noreply.github.com>
2026-03-21 04:00:35 +0800

    Upload lab 2 - challenge1.php

---
+++ Lab-2/challenge1.php
@@ -0,0 +1,13 @@
+<!-- Lab 2: Challenge 1 -->
+<!-- By: Faye Camille Buri -->
+
+<?php
+echo '<h3> Sum of An Array</h3>';
+
+$numbers = [1,2,3,4,5];
+$sum = array_sum($numbers);
+$quantity = count($numbers);
+
+echo "The sum of the $quantity numbers is: $sum";
+
+?>
