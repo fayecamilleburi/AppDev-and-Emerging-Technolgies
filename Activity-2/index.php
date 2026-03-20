157d66a48f01793dfccfee4136600d300176620e
Faye Camille Buri <152823567+fayecamilleburi@users.noreply.github.com>
2026-03-21 03:56:42 +0800

    Upload bootstrap activity 2

---
+++ Activity-2/index.php
@@ -0,0 +1,152 @@
+<html>
+    <head>
+        <title>CCL321-18</title>
+    </head>
+    <body>
+        <h1>Activity 2</h1>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+
+        <div class="container-fluid">
+            <a href="#">Messages<span class="badge">10</span></a><br/>
+            <a href="#">Notifications<span class="badge">30</span></a>
+            <button class="btn btn-primary">Click to see notifications<span class="badge">500</span></button>
+            
+            <div class="page=header">
+                <h3>Download <span class="label label-default">Version 10.1</span></h3>
+            </div>
+        </div>
+
+        <hr/>
+        <br/>
+        <br/>
+        <div class="container-fluid">
+            <div class="alert alert-danger fade in"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already exist!!!</div>
+            <div class="alert alert-success"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already exist!!!</div>
+            <div class="alert alert-warning"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already exist!!!</div>
+            <div class="alert alert-info"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already exist!!!</div>
+
+        </div>
+
+        <br/>
+        <br/>
+        <hr/>
+        <div class="container-fluid">
+            <div class="jumbotron">
+                <h1>Heading in jumbtron</h1>
+            </div>
+            <h1>Heading without jumbtron</h1>
+            <div class="page-header">
+                <h1>Welcome</h1>
+            </div>
+
+            <p class="well well-sm">This is the well</p>
+            <p class="well">This is the well</p>
+            <p class="well well-lg">This is the well</p>
+        </div>
+
+        <br/>
+        <br/>
+        <hr/>
+        <div class="container-fluid">
+            <span class="glyphicon glyphicon-trash"></span>:Trash Can<br/>
+            <span class="glyphicon glyphicon-pencil"></span>:Pencil<br/>
+            <button class="btn btn-success"><span class="glyphicon glyphicon-search"></span>Find</button>
+        </div>
+
+        <br/>
+        <br/>
+        <hr/>
+        <div class="container-fluid">
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College <span class="caret"></span></button>
+                <ul class="dropdown-menu">
+                    <li><a href="#">CCS</a></li>
+                    <li><a href="#">CET</a></li>
+                    <li><a href="#">CBA</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Exit</li>
+                    <li><a href="#">Logout</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College <span class="caret"></span></button>
+                <ul class="dropdown-menu dropdown-menu-right">
+                    <li><a href="#">CCS</a></li>
+                    <li><a href="#">CET</a></li>
+                    <li><a href="#">CBA</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Exit</li>
+                    <li><a href="#">Logout</a></li>
+                </ul>
+            </div>
+
+            <div class="dropup">
+                <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College <span class="caret"></span></button>
+                <ul class="dropdown-menu">
+                    <li><a href="#">CCS</a></li>
+                    <li><a href="#">CET</a></li>
+                    <li><a href="#">CBA</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Exit</li>
+                    <li><a href="#">Logout</a></li>
+                </ul>
+            </div>
+
+        <hr/>
+        <br/>
+        <div class="container-fluid">
+            <button class="btn btn-default">Click Me</button>
+            <button class="btn btn-primary btn-xs">Click Me</button>
+            <button class="btn btn-info btn-lg">Click Me</button>
+            <button class="btn btn-success btn-md">Click Me</button>
+            <button class="btn btn-warning">Click Me</button>
+            <button class="btn btn-danger btn-sm">Click Me</button>
+            <button class="btn btn-link">Click Me</button>
+            <button class="btn btn-danger btn-block">Click Me</button>
+            <button class="btn btn-danger disabled">Click Me</button>
+            <button class="btn btn-danger active">Click Me</button>
+
+            <div class="btn-group">
+                <button class="btn btn-info">Home</button>
+                <button class="btn btn-info">About Us</button>
+                <button class="btn btn-info">Contact Us</button>
+                <button class="btn btn-info">Products</button>
+            </div>
+
+            <div class="btn-group-vertical">
+                <button class="btn btn-info">Home</button>
+                <button class="btn btn-info">About Us</button>
+                <button class="btn btn-info">Contact Us</button>
+                <button class="btn btn-info">Products</button>
+            </div>
+
+            <div class="btn-group btn-group-justified">
+                <div class="btn-group">
+                    <button class="btn btn-success">Home</button>
+                </div>
+                <div class="btn-group">
+                    <button class="btn btn-success">About Us</button>
+                </div>
+                <div class="btn-group">
+                    <button class="btn btn-success">Contact Us</button>
+                </div>
+                <div class="btn-group">
+                    <button class="btn btn-success">Products</button>
+                </div>
+            </div>
+        </div>
+
+        <hr/>
+        <br/>
+        <div class="container-fluid">
+            <img src="pic/image.jpg">
+            <img src="pic/image.jpg" class="img-rounded">
+            <img src="pic/image.jpg" class="img-circle">
+            <img src="pic/image.jpg" class="img-thumbnail">
+        </div>
+    </body>
+</html>
