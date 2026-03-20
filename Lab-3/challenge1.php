4497ca7d5cde290709378d84da73849a4e27aaaa
Faye Camille Buri <152823567+fayecamilleburi@users.noreply.github.com>
2026-03-21 04:01:56 +0800

    Upload lab 3 - challenge1.php

---
+++ Lab-3/challenge1.php
@@ -0,0 +1,11 @@
+<!-- Lab 3: Challenge 1 -->
+<!-- By: Faye Camille Buri -->
+
+<?php
+for($i = 1; $i <= 10; $i++) {
+    for($j = 1; $j <= 10; $j++) {
+        echo "$i x $j = " . $i * $j . '<br>';
+    }
+}
+
+?>
